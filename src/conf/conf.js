16b19b64c8375dca0c1fe08206573c121c313867
prashanta mondal <prashantaa@gmail.com>
2026-02-15 13:52:56 +0530

    0.0.5v --Configure send data of a addmition user

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-    appwrite_Url: String(import.meta.env.VITE_APPWRITE_URL)||"",
-    appwrite_ProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID)||"",
-    appwrite_DatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID)||"",
-    appwrite_CollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTIN_ID)|'',
-    appwrite_BucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)||""
+    appwrite_Url: String(import.meta.env.VITE_APPWRITE_URL) || "",
+    appwrite_ProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID) || "",
+    appwrite_DatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID) || "",
+    appwrite_CollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID) || "",  // fixed
+    appwrite_BucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID) || ""
 };
 
 export default conf;
